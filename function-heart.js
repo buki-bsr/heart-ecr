a307f20007ca43af6f2d80532131342efbbb1fd7
buki-bsr <bugrakmsz@gmail.com>
2025-06-24 04:36:18 +0300

    Update function-heart.js

--- function-heart.js
+++ function-heart.js
@@ -8,119 +8,122 @@ M = Math;
 R = M.random;
 C = M.cos;
 Y = 6.3;
-for( i = 0; i <Y; i+= .2 ) { 
-	h.push([
-		O/2 + 180*M.pow(M.sin(i), 3),
-		Q/2 + 10 * (-(15*C(i) - 5*C(2*i) - 2*C(3*i) - C(4*i)))
-	])
+for (i = 0; i < Y; i += .2) {
+    h.push([
+        O / 2 + 180 * M.pow(M.sin(i), 3),
+        Q / 2 + 10 * (-(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i)))
+    ])
 }
 
 i = 0;
-while (i < v ) {
+while (i < v) {
+    x = R() * O;
+    y = R() * Q;
 
-	x = R() * O;
-	y = R() * Q;
-	//r = R() * 50 + 200;
-	//b = R() * r;
-	//g = R() * b;
+    H = i / v * 80 + 280;
+    S = R() * 40 + 60;
+    B = R() * 60 + 20;
 
-	H = i/v * 80 + 280;
-	S = R() * 40 + 60;
-	B = R() * 60 + 20;
+    f = [];
 
-	f = []; // create new trail
+    k = 0;
+    while (k < v) {
+        f[k++] = {
+            x: x,
+            y: y,
+            X: 0,
+            Y: 0,
+            R: (1 - k / v) + 1,
+            S: R() + 1,
+            q: ~~(R() * v),
+            D: i % 2 * 2 - 1,
+            F: R() * .2 + .7,
+            f: "hsla(" + ~~H + "," + ~~S + "%," + ~~B + "%,.5)"
+        }
+    }
 
-	k = 0;
-	while ( k < v ) { 
-		f[k++] = { // create new particle
-			x : x, // position 
-			y : y,
-			X : 0, // velocity
-			Y : 0,
-			R : (1 - k/v)  + 1, // radius
-			S : R() + 1, // acceleration 
-			q : ~~(R() * v), // target node on heart
-			//D : R()>.5?1:-1,
-			D : i%2*2-1, // direction around heart 
-			F : R() * .2 + .7, // friction
-			f : "hsla("+~~H+","+~~S+"%,"+~~B+"%,.5)" // colour
-		}
-	}
-
-	e[i++] = f; 
+    e[i++] = f;
 }
 
-function render(_) { 
-	a.fillStyle = _.f;
-	a.beginPath();
-	a.arc(_.x, _.y, _.R, 0, Y, 1);
-	a.closePath();
-	a.fill();
+function render(_) {
+    a.fillStyle = _.f;
+    a.beginPath();
+    a.arc(_.x, _.y, _.R, 0, Y, 1);
+    a.closePath();
+    a.fill();
 }
 
-function loop(){
+function loop() {
+    a.fillStyle = "rgba(0, 0, 0, 0.6)";
+    a.fillRect(0, 0, O, Q);
 
-	a.fillStyle = "rgba(0,0,0,.2)"; 
-	a.fillRect(0,0,O,Q);
+    i = v;
+    while (i--) {
+        f = e[i];
+        u = f[0];
+        q = h[u.q];
+        D = u.x - q[0];
+        E = u.y - q[1];
+        G = M.sqrt((D * D) + (E * E));
 
-	i = v;
-	while (i--) {
+        if (G < 10) {
+            if (R() > .95) {
+                u.q = ~~(R() * v);
+            } else {
+                if (R() > .99) u.D *= -1;
+                u.q += u.D;
+                u.q %= v;
+                if (u.q < 0) u.q += v;
+            }
+        }
 
-		f = e[ i ]; // get worm
-		u = f[ 0 ]; // get 1st particle of worm
-		q = h[ u.q ]; // get current node on heart path
-		D = u.x - q[0]; // calc distance
-		E = u.y - q[1];
-		G = M.sqrt( (D * D) + (E * E) );
-		
-		if ( G < 10 ) { // has trail reached target node?
-			if (R() > .95 ) { // randomly send a trail elsewhere
-				u.q = ~~(R() * v);
-			} else {
-				if ( R() > .99) u.D *= -1; // randomly change direction
-				u.q += u.D;
-				u.q %= v;
-				if ( u.q < 0 ) u.q += v;
-			}
-		}
+        u.X += -D / G * u.S;
+        u.Y += -E / G * u.S;
 
-		u.X += -D / G * u.S; // calculate velocity
-		u.Y += -E / G * u.S;
+        u.x += u.X;
+        u.y += u.Y;
 
-		u.x += u.X; // apply velocity
-		u.y += u.Y;
+        render(u);
 
-		render(u); // draw the first particle
+        u.X *= u.F;
+        u.Y *= u.F;
 
-		u.X *= u.F; // apply friction
-		u.Y *= u.F;
+        k = 0;
+        while (k < v - 1) {
+            T = f[k];
+            N = f[++k];
 
-		k = 0;
-		while ( k < v-1 ) { // loop through remaining dots
-			
-			T = f[ k ]; // this particle
-			N = f[ ++k ]; // next particle
+            N.x -= (N.x - T.x) * .7;
+            N.y -= (N.y - T.y) * .7;
 
-			N.x -= (N.x - T.x) * .7; // use zenos paradox to create trail
-			N.y -= (N.y - T.y) * .7;
+            render(N);
+        }
+    }
 
-			render(N);
+    // 💖 Yazıyı kalbin merkezine ekle
+    a.fillStyle = "rgba(231, 12, 158, 0.7)";
+	a.fillStyle ="text-shadow: 0 0 10px rgba(255, 255, 255, 0.7);";
+    a.font = "90px Gwendolyn";
+    a.textAlign = "center";
+    a.fillText("Yağmur", O / 2, Q / 2 + 10);
+}
 
-		}
-
-	}
-}; // eo loop()
-
-(function doit(){
-	requestAnimationFrame(doit);
-	loop();
+(function doit() {
+    requestAnimationFrame(doit);
+    loop();
 }());
-window.addEventListener('wheel', function(e) {
+
+window.addEventListener('resize', function () {
+    O = c.width = innerWidth;
+    Q = c.height = innerHeight;
+});
+
+window.addEventListener('wheel', function (e) {
     if (e.ctrlKey) {
-        e.preventDefault(); // Ctrl + Scroll ile zoom'u engeller
+        e.preventDefault();
     }
 }, { passive: false });
 
-document.addEventListener('gesturestart', function(e) {
-    e.preventDefault(); // Mobil pinch-to-zoom'u engeller
+document.addEventListener('gesturestart', function (e) {
+    e.preventDefault();
 });
